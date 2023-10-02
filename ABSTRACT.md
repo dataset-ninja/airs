0c3307448efecfad6c152bb48a559b4482ef9dc7
erastov-alex <erastov.alex@gmail.com>
2023-10-02 19:11:55 +0300

    upd ABSTRACT, settings

---
+++ ABSTRACT.md
@@ -0,0 +1,12 @@
+Authors present a new large-scale benchmark dataset termed Aerial Imagery for Roof Segmentation (AIRS). This dataset provides a wide coverage of aerial imagery with 7.5 cm resolution and contains over 220,000 buildings. The task posed for AIRS is defined as roof segmentation. 
+
+All the aerial images within the AOI are first merged into a single mosaic and then tiled into smaller images for better handling. The tiled images feature a size of 10000 × 10000 pixels, with a 10% overlap between adjacent tiles. As a result, the whole region is tiled into 1047 images. To facilitate research, authors split the dataset by randomly assigning the images to the following three subsets:
+- *train* set. This set contains 857 images and the corresponding roof
+labels for training.
+- *val* set. This set contains 94 images and the corresponding
+roof labels for validation.
+- *test* set. This set contains 96 images for benchmark testing. Currently, for fair comparison, the corresponding roof labels are not disclosed. They will be used for evaluating the results submitted to the public challenge centered on the AIRS dataset.
+
+![Fig](https://i.ibb.co/RBDgPCL/Screenshot-2023-10-02-190852.png)
+
+This figure displays the study area of the AIRS dataset. The area of interest (AOI) covers about 457 km², including almost the full area of Christchurch, the largest city in the South Island of New Zealand. The aerial imagery and the corresponding building map are open-source data provided by LINZ Data Service. The photographs were captured during the flying seasons of 2015 and 2016, and the open-source images were ortho-rectified DOMs with RGB channels and 7.5 cm resolution in the projection of New Zealand Transverse Mercator. The building footprints, which are stored as 2D polygons in a shapefile, were produced in June 2016 by manual annotation.
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,37 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "AIRS"
+PROJECT_NAME_FULL: str = "AIRS: Aerial Imagery for Roof Segmentation"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.UrbanPlanning(is_used=False),Industry.Environmental()]
+CATEGORY: Category = Category.Surveillance(extra=Category.Aerial())
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.SemanticSegmentation()]
+ANNOTATION_TYPES: List[AnnotationType] = AnnotationType.SemanticSegmentation()
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2018
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/atilol/aerialimageryforroofsegmentation"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = None
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/airs"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = ["https://www.kaggle.com/datasets/atilol/aerialimageryforroofsegmentation"]
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,15 +51,33 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = ["https://www.sciencedirect.com/science/article/pii/S0924271618303083"]
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
 REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://www.sciencedirect.com/science/article/pii/S0924271618303083"
+AUTHORS: Optional[List[str]] = [
+    "Qi Chena",
+    "Lei Wangb",
+    "Yifan Wud",
+    "Guangming Wuc",
+    "Zhiling Guoc",
+    "Steven L. Waslander",
+    ]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "China University of Geosciences (Wuhan)",
+    "University of Waterloo",
+    "University of Tokyo",
+    "AtlasAI Inc",
+    ]
+
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://en.cug.edu.cn/",
+    "https://uwaterloo.ca/content/home",
+    "https://www.u-tokyo.ac.jp/en/",
+    "https://www.atlasai.co/",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
